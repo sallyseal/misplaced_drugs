afc8434e7f1681cba7867ddd8a8dae7e5ec314e8
Adam <apmills@outlook.com>
2018-02-13 20:54:11 +0000

    The website now actually runs and you can see some (placeholder) information at /drug/1 and /target/1. The number is the primary key of whatever you want to look at.

--- misplaced_drugs/drugs/repos/views.py
+++ misplaced_drugs/drugs/repos/views.py
@@ -7,20 +7,22 @@ from .models import Drug, Target
 # Create your views here.
 class DrugView(generic.DetailView):
     model = Drug
-    template_name = 'polls/drug.html'
+    template_name = 'repos/drug.html'
+    context_object_name = 'this_drug'
 
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    # def get_queryset(self):
+    #     """
+    #     Excludes any questions that aren't published yet.
+    #     """
+    #     return Drug.objects.all()
 
 class TargetView(generic.DetailView):
     model = Target
-    template_name = 'polls/target.html'
+    template_name = 'repos/target.html'
+    context_object_name = 'this_target'
 
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    # def get_queryset(self):
+    #     """
+    #     Excludes any questions that aren't published yet.
+    #     """
+    #     return Target.objects.all()
