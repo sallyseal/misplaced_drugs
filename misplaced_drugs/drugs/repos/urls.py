e65b677b50b70ae49a53207d6ab1d01965eff146
sallyseal <macoomer@gmail.com>
2018-02-16 14:11:54 +0000

    Adding css to the html files

--- misplaced_drugs/drugs/repos/urls.py
+++ misplaced_drugs/drugs/repos/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'repos'
 urlpatterns = [
-    path('drug/<int:pk>/', views.DrugView.as_view(), name='drug'),
-    path('target/<int:pk>/', views.TargetView.as_view(), name='target'),
+    path('drug/<str:pk>/', views.DrugView.as_view(), name='drug'),
+    path('target/<str:pk>/', views.TargetView.as_view(), name='target'),
 ]
