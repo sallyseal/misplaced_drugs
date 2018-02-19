fad610d639f006991963c95ee618c9db1ed78a98
Adam <apmills@outlook.com>
2018-02-19 11:42:57 +0000

    Populate DB command now takes in all the information in our database.
    We also have a home page and an about page at /repos/ and /repos/about

--- misplaced_drugs/drugs/repos/management/commands/populate_db.py
+++ misplaced_drugs/drugs/repos/management/commands/populate_db.py
@@ -12,15 +12,20 @@ class Command(BaseCommand):
         seen_targets = []
         infile = open(filename, 'r')
         for line in infile.readlines():
-            sline = line.split(',')
-            if len(sline[4]) > 7:
+            sline = line.split('\t')
+            if len(sline[6]) > 7:
                 continue
-            dbid = sline[4]
-            gen_name = sline[2]
-            brand_name = sline[3].split('#')[0]
-            uniprot = sline[8]
-            prot_name = sline[7]
-            pdb = sline[9]
+            dbid = sline[6]
+            gen_name = sline[4]
+            brand_name = sline[4].split('#')[0]
+            approval = sline[17]
+            indication = sline[18]
+            moa = sline[19]
+            chembl = sline[20]
+            uniprot = sline[10]
+            prot_name = sline[9]
+            pdb = sline[11]
+            gene = sline[22].rstrip()
 
             if dbid in seen_drugs:
                 known_drug = True
@@ -32,7 +37,7 @@ class Command(BaseCommand):
                 known_target = False
 
             if known_drug and known_target:
-                # Just add interaction
+                # Just add an interaction link between a drug and target
                 for drug in Drug.objects.all():
                     if drug.drugbank_ID == dbid:
                         d = drug
@@ -43,31 +48,31 @@ class Command(BaseCommand):
                 d.save()
                 self.stdout.write(self.style.SUCCESS('Added new interaction...'))
             elif known_drug:
-                # Add new target to existing drug, add interaction
+                # Add new target to existing drug
                 for drug in Drug.objects.all():
                     if drug.drugbank_ID == dbid:
                         d = drug
-                targ = Target(uniprot_ID=uniprot, protein_name=prot_name, PDB_ID=pdb)
+                targ = Target(uniprot_ID=uniprot, protein_name=prot_name, PDB_ID=pdb, gene_name=gene)
                 targ.save()
                 d.targets.add(targ)
                 seen_targets.append(uniprot)
                 self.stdout.write(self.style.SUCCESS('Added new target...'))
             elif known_target:
-                # Add new drug to existing target, add interaction
+                # Add new drug to existing target
                 for target in Target.objects.all():
                     if target.uniprot_ID == uniprot:
                         t = target
-                drug = Drug(drugbank_ID=dbid, generic_name=gen_name, brand_name=brand_name)
+                drug = Drug(drugbank_ID=dbid, generic_name=gen_name, brand_name=brand_name, approval=approval, indication=indication, moa=moa, chembl_ID=chembl)
                 drug.save()
                 drug.targets.add(t)
                 drug.save()
                 seen_drugs.append(dbid)
                 self.stdout.write(self.style.SUCCESS('Added new drug...'))
             else:
-                # Both drug and target are new, add all three
-                targ = Target(uniprot_ID=uniprot, protein_name=prot_name, PDB_ID=pdb)
+                # Both drug and target are new, add both
+                targ = Target(uniprot_ID=uniprot, protein_name=prot_name, PDB_ID=pdb, gene_name=gene)
                 targ.save()
-                drug = Drug(drugbank_ID=dbid, generic_name=gen_name, brand_name=brand_name)
+                drug = Drug(drugbank_ID=dbid, generic_name=gen_name, brand_name=brand_name, approval=approval, indication=indication, moa=moa, chembl_ID=chembl)
                 drug.save()
                 drug.targets.add(targ)
                 drug.save()
