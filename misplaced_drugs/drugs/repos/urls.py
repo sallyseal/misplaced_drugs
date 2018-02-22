c2ad3a605c0810fa81448ea9b6faaa8bf3f213cb
Adam <apmills@outlook.com>
2018-02-22 13:35:52 +0000

    Added autocomplete search bar.
    This includes a new .js file and some changes to the CSS styling. Also we now have a forms.py file and some changes to
    the views.

--- misplaced_drugs/drugs/repos/urls.py
+++ misplaced_drugs/drugs/repos/urls.py
@@ -6,6 +6,6 @@ app_name = 'repos'
 urlpatterns = [
     path('drug/<str:pk>/', views.DrugView.as_view(), name='drug'),
     path('target/<str:pk>/', views.TargetView.as_view(), name='target'),
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.homeView, name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
--- misplaced_drugs/drugs/repos/views.py
+++ misplaced_drugs/drugs/repos/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Drug, Target
+from dal import autocomplete
+from .forms import SearchForm
+try:
+    from django.urls import reverse_lazy
+except ImportError:
+    from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 class DrugView(generic.DetailView):
@@ -26,3 +32,22 @@ class HomeView(generic.base.TemplateView):
 
 class AboutView(generic.base.TemplateView):
     template_name = 'repos/about.html'
+
+def homeView(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            drug_name = form.cleaned_data['your_name']
+            d = get_object_or_404(Drug, generic_name=drug_name)
+            # redirect to a new URL:
+            return HttpResponseRedirect('/repos/drug/' + d.drugbank_ID)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    return render(request, 'repos/home.html', {'form': form})
