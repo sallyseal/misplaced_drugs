a3ffcb327e739274fa6f3514274900de178f02cf
sallyseal <macoomer@gmail.com>
2018-03-07 14:00:30 +0000

    html and css messed in the settings.py folder

--- misplaced_drugs/drugs/drugs/settings.py
+++ misplaced_drugs/drugs/drugs/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_ROOT = "~/software/misplaced_drugs/misplaced_drugs/drugs/repos/static"
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
